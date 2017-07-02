2bfa51cf5f917441f8eae6d3ea1d06ff915666c0
kaolase <1121861540@qq.com>
2017-07-02 14:55:24 +0800

    unity

---
+++ s1/s1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace s1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("你哈大轰大嗡");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ song/song/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace song
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int day = 365;
+            int bai = day / 100;
+            int shi = (day%100) / 10;
+            int ge = day%10;
+            Console.WriteLine("百位是：" + bai + "十位是：" + shi + "个位是：" + ge);
+            Console.ReadKey();
+              */
+        }
+        }
+    }
+
